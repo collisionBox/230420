ab8afee9cee1bfd5dc1837194d536e427e989b6f
collisionBox <106565692+collisionBox@users.noreply.github.com>
2023-04-19 10:58:46 +0900

    gameFile

---
+++ ソース/PlayScene.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "SceneBase.h"
+
+#include "camera.h"
+#include "PlayerBody.h"
+#include "MapManager.h"
+
+class PlayScene :
+    public SceneBase
+{
+public:
+    PlayScene(int mapNum);
+    ~PlayScene();
+    SceneBase* Update(float deltaTime);
+    void Draw();
+private:
+    void CheckWinner();// Ÿ—˜Ò‚ğŒŸõ.
+    bool IsChangeResultScene();
+    bool WaitChangeSceneTime(float deltaTime);
+    void PlayerInit();
+
+    PlayerBody* player[2];
+    int battleNum;
+    const int maxBattleNum = 3;// Å‘å‘Îí‰ñ”.
+    const int maxWinNum = 2;// Å‘åŸ—˜”.
+    const float OnShootingDownWaitTime = 10.0f;// ”íŒ‚’Ä‘Ò‹@ŠÔ.
+    float deltaWaitTime;// ‘Ò‹@ŠÔ.
+};
+
